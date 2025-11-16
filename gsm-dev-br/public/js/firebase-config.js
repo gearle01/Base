c12e1e6126c0a2ed736f8a319f0593174e423da2
gearle01 <47864743+gearle01@users.noreply.github.com>
2025-11-16 19:46:16 -0300

    cors corrigido

--- gsm-dev-br/public/js/firebase-config.js
+++ gsm-dev-br/public/js/firebase-config.js
@@ -6,7 +6,7 @@ window.firebaseConfig = {
   apiKey: "AIzaSyB9KwQm9eZqqkOh8S4hSBiy3RLlNFyQ4fk",
   authDomain: "base-5488a.firebaseapp.com",
   projectId: "base-5488a",
-  storageBucket: "base-5488a.appspot.com",
+  storageBucket: "base-5488a.firebasestorage.app",  // ← Verifique se é este
   messagingSenderId: "399469780607",
   appId: "1:399469780607:web:39690561538a9e9a0a694a",
   measurementId: "G-JJ259S5NTX"
